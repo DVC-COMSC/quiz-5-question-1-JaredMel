4ddfad1677d4b1ff63ea52a6135ae8eaab7c42d3
JaredMel <jareddmelendez1764@gmail.com>
2024-03-08 13:43:53 -0800

    WorkinProgress

--- main.cpp
+++ main.cpp
@@ -4,6 +4,20 @@ using namespace std;
 int main()
 {
    
-   /* Complete your code */
+   int N;
+   cout << "Input: ";
+   cin >> N;
+
+   for (size_t i = 0; i < N; i++)
+   {
+      char letter = 65;
+      for (size_t j = 0; j <= i; j++)
+      {
+         cout << letter;
+         letter++;
+      }
+      
+   }
+   
   
 }
